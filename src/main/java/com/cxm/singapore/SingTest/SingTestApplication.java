c077137fb3ef22662d2f01b25bd0ee754b26e653
Simon <simon.xianming@ext.deposit-solution.com>
2025-07-28 16:41:35 +0800

    Initial commit

---
+++ src/main/java/com/cxm/singapore/SingTest/SingTestApplication.java
@@ -0,0 +1,13 @@
+package com.cxm.singapore.SingTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cxm/singapore/SingTest/SingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxm.singapore.SingTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
